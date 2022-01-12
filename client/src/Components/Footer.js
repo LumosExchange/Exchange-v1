9f09e06c8e52c6a0f4881da378e68e31fb8d4cae
mekeirc <mekeirc@gmail.com>
2022-01-12 13:14:32 +0000

    Update footer

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,11 +1,19 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import Heading from "./Heading";
+import Paragraph from "./Paragraph";
 
 const FooterBase = styled.footer(({ theme }) => css`
 	background: ${theme.colors.black};
 	border-top: 1px solid ${theme.colors.grey};
 
+	p {
+		font-size: 14px;
+		
+		@media screen and (min-width: ${theme.breakpoints.sm}){
+			font-size: 18px;
+		}
+	}
 	a {
 		color: ${theme.colors.blueGrey};
 		font-size: 14px;
@@ -25,37 +33,36 @@ const FooterBase = styled.footer(({ theme }) => css`
 const Footer = () => (
     <FooterBase>
 		<div className="container-fluid py-5">
-			<div className="row">
 				<div className="col-12 d-flex flex-column">
-					<Heading size="20px" bold>Sitemap</Heading>
 					<div className="row">
-						<div className="col-6 col-sm-3 d-flex flex-column">
+						<div className="col-6 col-sm-3 d-flex flex-column mb-5 mb-md-0">
+							<Heading size="20px" bold>Sitemap</Heading>
 							<a href="/About" alt="About">About</a>
-							<a href="/About" alt="About">Solutions</a>
-							<a href="/About" alt="About">Investing</a>
-							<a href="/About" alt="About">Team</a>
-							<a href="/About" alt="About">Careers</a>
-							<a href="/About" alt="About">Contact</a>
+							<a href="/Solutions" alt="Solutions">Solutions</a>
+							<a href="/Investing" alt="Investing">Investing</a>
+							<a href="/Team" alt="Team">Team</a>
+							<a href="/Careers" alt="Careers">Careers</a>
+							<a href="/Contract" alt="Contract">Contact</a>
 						</div>
 						<div className="col-6 col-sm-3 d-flex flex-column">
-							<a href="/About" alt="About">NFTs</a>
-							<a href="/About" alt="About">Partners</a>
-							<a href="/About" alt="About">Library</a>
-							<a href="/About" alt="About">Roadmap</a>
-							<a href="/About" alt="About">Greenmap</a>
-							<a href="/About" alt="About">Whitepaper</a>
+							<a href="/NFTs" alt="NFTs">NFTs</a>
+							<a href="/Partners" alt="Partners">Partners</a>
+							<a href="/Library" alt="Library">Library</a>
+							<a href="/Roadmap" alt="Roadmap">Roadmap</a>
+							<a href="/Greenmap" alt="Greenmap">Greenmap</a>
+							<a href="/Whitepaper" alt="Whitepaper">Whitepaper</a>
 						</div>
-						<div className="col-12 col-sm-3 d-flex flex-column pt-5">
+						<div className="col-12 col-sm-3 d-flex flex-column mb-5 mb-md-0">
 							<Heading size="20px" bold>Legal</Heading>
-							<a href="/About" alt="About">About</a>
+							<a href="/Terms" alt="Terms">Our Terms</a>
+							<a href="/Privacy" alt="Privacy">Your Privacy</a>
 						</div>
-						<div className="col-4 d-flex flex-column">
+						<div className="col-12 col-sm-3 d-flex flex-column">
 							<Heading size="20px" bold>Subscribe to our newsletter</Heading>
-							<a href="/About" alt="About">About</a>
+							<Paragraph size="18px" color="blueGrey">Get regular project updates and access to new events on Discord, Twitter or in the real world.</Paragraph>
 						</div>
 					</div>
 				</div>
-			</div>
 		</div>
     </FooterBase>
 );
