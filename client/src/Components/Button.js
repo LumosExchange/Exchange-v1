ac3f98e5290082c556ee9e60841211bb6cb710c8
mekeirc <mekeirc@gmail.com>
2022-01-13 16:32:24 +0000

    Build homepage, add images for homepage, style

--- client/src/Components/Button.js
+++ client/src/Components/Button.js
@@ -1,19 +1,23 @@
 import PropTypes from 'prop-types';
 import styled, { css } from "styled-components";
 
-const ButtonBase = styled.button(({ theme, round, size }) => css`
-	background: ${theme.colors.yellow};
+const ButtonBase = styled.button(({ 
+    theme, round, size, color, textColor, boldText, fontSize
+}) => css`
+	background: ${theme.colors[color]};
 	border-radius: ${round ? '50px' : '20px'};
 	border: 0;
-	font-size: 20px;
+	font-size: ${fontSize ? fontSize : '20px'};
     width: 100%;
-	color: ${theme.colors.black};
-	font-family: 'THICCCBOI-BOLD';
+	color: ${theme.colors[textColor]};
+	font-family: ${boldText ? 'THICCCBOI-BOLD' : 'THICCCBOI-REGULAR'};
 	padding: ${
 		(size === 'sm' && '8px')
 		|| (size === 'md' && '10px 30px')
 		|| (size === 'lg' && '16px')
 	};
+
+    &:hover { transform: scale(1.05); }
 
     @media screen and (min-width: ${theme.breakpoints.md}) {
         width: auto;
@@ -23,12 +27,14 @@ const ButtonBase = styled.button(({ theme, round, size }) => css`
 const PrimaryButton = ({
     text, size, hasIcon, 
     iconPosition, iconName, className,
-    onClick, type, form, value
+    onClick, type, form, value, color,
+    textColor, round, boldText, fontSize
 }) => (
     <ButtonBase
         onClick={onClick} type={type} form={form} value={value}
-        size={size}
-        className={`d-flex align-items-center justify-content-center ${className}`}
+        size={size} color={color} textColor={textColor} 
+        round={round} boldText={boldText} fontSize={fontSize}
+        className={`d-flex align-items-center justify-content-center ${className ? className : ''}`}
     >
         {(hasIcon) && (iconPosition === 'left') && (
             <i className="material-icons me-3">{iconName}</i>
@@ -45,6 +51,9 @@ PrimaryButton.propTypes = {
 	round: PropTypes.bool,
     text: PropTypes.string,
     hasIcon: PropTypes.bool,
+    color: PropTypes.string,
+    textColor: PropTypes.string,
+    boldText: PropTypes.bool,
 }
 
 PrimaryButton.defaultProps = {
@@ -54,6 +63,9 @@ PrimaryButton.defaultProps = {
     hasIcon: false,
     iconPosition: 'right',
     iconName: 'arrow_forward',
+    color: 'yellow',
+    textColor: 'black',
+    boldText: true,
 }
 
 export default PrimaryButton;
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,181 @@
 import React from "react";
+import styled, { css } from "styled-components";
+import { FormBody } from "../Components/FormInputs";
+import Heading from "../Components/Heading";
 
-function Home() {
-    return <div>HELLO HOME PAGE</div>
+// Images
+import ShowcaseTablet from '../Images/showcase-tablet.svg';
+import ShowcaseLaptop from '../Images/showcase-laptop.svg';
+import GradientButton from "../Components/GradientButton";
+import PrimaryButton from "../Components/Button";
+import Paragraph from "../Components/Paragraph";
+import IconSolana from '../Images/icon-circle-solana.svg';
+import IconLumosRewards from '../Images/icon-circle-lumos-rewards.svg';
+import IconKin from '../Images/icon-circle-kin.svg';
+import IconCope from '../Images/icon-circle-cope.svg';
+import IconMiddleMan from '../Images/icon-middleman.svg';
 
+const ShowcaseBase = styled.div(({ theme }) => css`
+    min-height: 600px;
+    .tablet {
+        left: 0;
+        bottom: 0;
+        position: absolute;
+        z-index: 2;
+    }
+    .laptop {
+    }
+`);
 
-}
+const Card = styled.div(({ theme }) => css`
+    background: ${theme.colors.darkerGrey};
+    border-radius: 20px;
+`);
+
+const IconCard = ({ icon, title, text}) => (
+    <div className="col-10 col-sm-6 col-md-6 col-xl-3 mb-4 mb-xl-0">
+        <Card className="p-4 text-center mb-4 mb-xl-0 h-100">
+            <img src={icon} alt={title}  className="mb-3" />
+            <Heading color="white" size="24px" bold>{title}</Heading>
+            <Paragraph color="white" size="14px">
+                {text}
+            </Paragraph>
+        </Card>
+    </div>
+);
+
+const Showcase = () => (
+    <ShowcaseBase className="position-relative mb-5">
+        <img className="tablet img-fluid" src={ShowcaseTablet} alt="Showcase Tablet" />
+        <img className="laptop img-fluid" src={ShowcaseLaptop} alt="Showcase Tablet" />
+    </ShowcaseBase>
+);
+
+const Home = () => (
+    <FormBody className="container-fluid">
+        <div className="row pt-5 d-flex justify-content-center">
+            <div className="col-12 col-xl-4 d-flex align-items-center justify-content-center">
+                <div className="my-5">
+                    <Heading color="white" size="48px" bold>
+                        Non-Custodial,<br />
+                        Peer-to-Peer Trading
+                    </Heading>
+                    <div className="row mt-5 pt-5">
+                        <div className="col-12 col-md-6">
+                            <GradientButton
+                                text="Sign Up"
+                                className="w-100 mb-3"
+                                fontSize="20px"
+                            />
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <PrimaryButton
+                                className="w-100"
+                                round
+                                color="navyGrey"
+                                textColor="white"
+                                text="How it Works"
+                                boldText={false}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-12 col-xl-4 d-flex justify-content-center">
+                <Showcase />
+            </div>
+        </div>
+        <div className="container">
+            <div className="row justify-content-center mb-4">
+                <IconCard
+                    title="Solana"
+                    icon={IconSolana}
+                    text="The open-source crypto with the fastest responsive speed and rapidly growing ecosystem."
+                />
+                <IconCard
+                    title="Lumos Rewards"
+                    icon={IconLumosRewards}
+                    text="Our SPL asset focused on providing users an incentive to trade regularly, event participation & referrals."
+                />
+                <IconCard
+                    title="KIN"
+                    icon={IconKin}
+                    text="The open-source crypto with the fastest responsive speed and rapidly growing ecosystem."
+                />
+                <IconCard
+                    title="COPE"
+                    icon={IconCope}
+                    text="The open-source crypto with the fastest responsive speed and rapidly growing ecosystem."
+                />
+            </div>
+            <div className="row justify-content-between align-items-center py-6">
+                <div className="col-12 col-xl-4 mb-5 mb-xl-0">
+                    <Heading size="48px">
+                        Your Coins <br />
+                        In Your Seize
+                    </Heading>
+                    <Paragraph size="22px">Lumos Exchange is the first non-custodial peer-to-peer platform for Solana. We allow users trade with each other in safety, and protects your wallet from theft.</Paragraph>
+                    <PrimaryButton
+                        text="View The Marketplace"
+                        className="mt-5 p-0"
+                        color="black"
+                        textColor="yellow"
+                        onClick={null}
+                        hasIcon
+                        fontSize="28px"
+                    />
+                </div>
+                <div className="col-12 col-xl-8">
+                    <div className="row">
+                        <div className="col-12 col-lg-6">
+                            <Card className="d-flex p-4 mb-3">
+                                <img src={IconMiddleMan} alt="Middleman" />
+                                <div className="ms-4 d-flex flex-column justify-content-center">
+                                    <Heading size="24px">No Middlemen</Heading>
+                                    <Paragraph size="18px">
+                                        You exchange directly with another person, fast and easy. Say goodbye to slow middleman.
+                                    </Paragraph>
+                                </div>
+                            </Card>
+                        </div>
+                        <div className="col-12 col-lg-6">
+                            <Card className="d-flex p-4 mb-3">
+                                <img src={IconMiddleMan} alt="Middleman" />
+                                <div className="ms-4 d-flex flex-column justify-content-center">
+                                    <Heading size="24px">No Middlemen</Heading>
+                                    <Paragraph size="18px">
+                                        You exchange directly with another person, fast and easy. Say goodbye to slow middleman.
+                                    </Paragraph>
+                                </div>
+                            </Card>
+                        </div>
+                        <div className="col-12 col-lg-6">
+                            <Card className="d-flex p-4 mb-3">
+                                <img src={IconMiddleMan} alt="Middleman" />
+                                <div className="ms-4 d-flex flex-column justify-content-center">
+                                    <Heading size="24px">No Middlemen</Heading>
+                                    <Paragraph size="18px">
+                                        You exchange directly with another person, fast and easy. Say goodbye to slow middleman.
+                                    </Paragraph>
+                                </div>
+                            </Card>
+                        </div>
+                        <div className="col-12 col-lg-6">
+                            <Card className="d-flex p-4 mb-3">
+                                <img src={IconMiddleMan} alt="Middleman" />
+                                <div className="ms-4 d-flex flex-column justify-content-center">
+                                    <Heading size="24px">No Middlemen</Heading>
+                                    <Paragraph size="18px">
+                                        You exchange directly with another person, fast and easy. Say goodbye to slow middleman.
+                                    </Paragraph>
+                                </div>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </FormBody>
+);
 
 export default Home;
