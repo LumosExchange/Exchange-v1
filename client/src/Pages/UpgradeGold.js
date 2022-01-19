eea10391dabaf0737ea97d0d648feea0c3ad1d9e
ChristosDigkas <christos.digkas@gmail.com>
2022-01-19 15:24:03 +0100

    upgradegold
    asda

--- client/src/Pages/UpgradeGold.js
+++ client/src/Pages/UpgradeGold.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import "../App.css";
 import Axios from "axios";
+import { FormInput, FormBody } from "../Components/FormInputs";
+import Heading from "../Components/Heading";
+import { useNavigate } from "react-router";
 
-function UpgradeGold() {
+const UpgradeGold = () => {
+  const navigate = useNavigate();
   const [employerNameReg, setEmployerNameReg] = useState("");
   const [employerAddressReg, setEmployerAddressReg] = useState("");
 
@@ -10,86 +14,74 @@ function UpgradeGold() {
   const [incomeReg, setIncomeReg] = useState("");
 
   const upgradeGold = () => {
-    Axios.post("http://localhost:3001/upgradeGold", {
+    Axios.post("http://localhost:3001/UpgradeGold", {
       EmployerName: employerNameReg,
       EmployerAddress: employerAddressReg,
       Occupation: occupationReg,
       Income: incomeReg,
     });
+    navigate("/Home");
   };
 
   return (
-    <div class="form">
-      <div class="subtitle">
-        <h1>Let's upgrade to gold tier!</h1>
-      </div>
-      <div class="input-container ic1">
-        <label for="EmployerName" class="placeholder">
-          Employer Name
-        </label>
-        <input
-          id="Employer Name"
-          class="input"
-          type="text"
-          placeholder=" "
-          onChange={(e) => {
-            setEmployerNameReg(e.target.value);
-          }}
-        />
-        <div class="cut"></div>
-      </div>
+    <FormBody className="d-flex align-items-center justify-content-center py-5 container-fluid">
+      <div className="row">
+        <div className="col-11 col-md-6 d-flex flex-column m-auto">
+          <Heading size="36px" color="white" className="mt-5 mb-4 text-center">
+            Upgrade to gold
+          </Heading>
+          <Heading size="24px" color="white" className="mb-5 text-center">
+            Enter your details to upgrade to gold.
+          </Heading>
 
-      <div class="input-container ic2">
-        <label for="Employeraddress" class="placeholder">
-          Employer Address
-        </label>
-        <input
-          id="Employer address"
-          class="input"
-          type="text"
-          placeholder=" "
-          onChange={(e) => {
-            setEmployerAddressReg(e.target.value);
-          }}
-        />
-        <div class="cut"></div>
-      </div>
+          <form>
+            <FormInput
+              id="EmployerName"
+              className="mb-3 w-100"
+              type="text"
+              placeholder="Employer Name "
+              onChange={(e) => {
+                setEmployerNameReg(e.target.value);
+              }}
+            />
 
-      <div class="input-container ic2">
-        <label for="Occupation" class="placeholder">
-          Occupation
-        </label>
-        <input
-          id="Occupation"
-          class="input"
-          type="Occupation"
-          placeholder=" "
-          onChange={(e) => {
-            setOccupationReg(e.target.value);
-          }}
-        />
-        <div class="cut cut-short"></div>
-      </div>
-      <div class="input-container ic2">
-        <label for="Income" class="placeholder">
-          Estimated Income Range
-        </label>
-        <input
-          id="Income"
-          class="input"
-          type="Income"
-          placeholder=" "
-          onChange={(e) => {
-            setIncomeReg(e.target.value);
-          }}
-        />
-        <div class="cut cut-short"></div>
-      </div>
+            <FormInput
+              id="EmployerAddress"
+              className="mb-3 w-100"
+              type="text"
+              placeholder="Employer Address "
+              onChange={(e) => {
+                setEmployerAddressReg(e.target.value);
+              }}
+            />
 
-      <button type="text" class="submit" onClick={upgradeGold}>
-        upgrade
-      </button>
-    </div>
+            <FormInput
+              id="Occupation"
+              className="mb-3 w-100"
+              type="Occupation"
+              placeholder="Occupation "
+              onChange={(e) => {
+                setOccupationReg(e.target.value);
+              }}
+            />
+
+            <FormInput
+              id="Income"
+              className="mb-3 w-100"
+              type="Income"
+              placeholder="Income "
+              onChange={(e) => {
+                setIncomeReg(e.target.value);
+              }}
+            />
+
+            <button type="text" class="submit" onClick={upgradeGold}>
+              upgrade
+            </button>
+          </form>
+        </div>
+      </div>
+    </FormBody>
   );
-}
+};
 export default UpgradeGold;
