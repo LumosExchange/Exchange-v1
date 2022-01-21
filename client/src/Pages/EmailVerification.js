77c70e5efb93e2d071a5baf94278978f2b4041b3
Harry Greenway <harrygreenway96@gmail.com>
2022-01-21 00:23:57 +0000

    Built email verification
    built email verifcation just need to finish off return of auth

--- client/src/Pages/EmailVerification.js
+++ client/src/Pages/EmailVerification.js
@@ -7,48 +7,57 @@ import Card from "../Components/Card";
 import Axios from "axios";
 
 const EmailVerification = () => {
-    const [Twofa, setTwofaCode] = useState("");
-    const [verified, setVerifed] = useState("");
-    const [secret, setSecret] = useState("");
+  const { state } = useLocation();
 
-    async function VerifyEmailAuth(event) {
-        event.preventDefault();
-        Axios.get("http://localhost:3001/VerifyEmail2FA", {
-          params: {
-            secret: secret.base32,
-            passcode: Twofa,
-          },
-        })
-        .then((response) => {
-          setVerifed(response.data);
-          console.log("authentication is: " + verified);
-        });
-    }
+  const [Twofa, setTwofaCode] = useState("");
+  const [verified, setVerifed] = useState("");
+  const [userEmail, setUserEmail] = useState([]);
 
-    return (
-    	<PageBody className="d-flex align-items-center justify-content-center py-5 container-fluid flex-column">
-			<Card radius="20px" color="darkerGrey" className="p-5 d-flex flex-column">
-				<Heading className="pb-4 text-center">Please enter your email 2FA code below</Heading>
-				<div className="w-100 row">
-					<div className="col-12 col-md-8">
-						<FormInput
-							type="text"
-							id="Code"
-							name="code"
-							placeholder="Enter 2FA Code"
-							onChange={(e) => {
-								setTwofaCode(e.target.value);
-							}}
-							className="w-100"
-						/>
-					</div>
-					<div className="col-12 col-md-4 p-0">
-						<PrimaryButton type="submit" text="Submit" onClick={VerifyEmailAuth} className="w-100 h-100" />
-					</div>
-				</div>
-			</Card>
-    	</PageBody>
-    );
-}
+  async function VerifyEmailAuth(event) {
+    event.preventDefault();
+    useEffect(() => {
+      Axios.get("http://localhost:3001/VerifyEmail2FA", {
+        params: {
+          email: state.email,
+          passcode: TwoFa,
+        },
+      }).then((response) => {
+        setUserEmail(response.data);
+      });
+    }, []);
+  }
+
+  return (
+    <PageBody className="d-flex align-items-center justify-content-center py-5 container-fluid flex-column">
+      <Card radius="20px" color="darkerGrey" className="p-5 d-flex flex-column">
+        <Heading className="pb-4 text-center">
+          Please enter your email 2FA code below
+        </Heading>
+        <div className="w-100 row">
+          <div className="col-12 col-md-8">
+            <FormInput
+              type="text"
+              id="Code"
+              name="code"
+              placeholder="Enter 2FA Code"
+              onChange={(e) => {
+                setTwofaCode(e.target.value);
+              }}
+              className="w-100"
+            />
+          </div>
+          <div className="col-12 col-md-4 p-0">
+            <PrimaryButton
+              type="submit"
+              text="Submit"
+              onClick={VerifyEmailAuth}
+              className="w-100 h-100"
+            />
+          </div>
+        </div>
+      </Card>
+    </PageBody>
+  );
+};
 
 export default EmailVerification;
