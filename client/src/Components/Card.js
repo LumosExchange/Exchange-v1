0f997afde11c5555be99d2bd6a4ec66a44c2f282
mekeirc <mekeirc@gmail.com>
2022-01-26 12:35:53 +0000

    Start initial build of buy page, update card components + props

--- client/src/Components/Card.js
+++ client/src/Components/Card.js
@@ -1,18 +1,20 @@
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Card = styled.div(({ theme, color }) => css`
+const Card = styled.div(({ theme, color, radius }) => css`
     background: ${theme.colors[color]};
     color: ${theme.colors.white};
-    border-radius: 20px;
+    border-radius: ${radius};
 `);
 
 Card.propTypes = {
     color: PropTypes.string,
+    radius: PropTypes.string,
 }
 
 Card.defaultProps = {
     color: 'grey',
+    radius: '20px',
 }
 
 export default Card;
