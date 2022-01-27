f5757f23412bbc6d01bb9e9b87d222778a67b114
Harry Greenway <harrygreenway96@gmail.com>
2022-01-27 15:29:19 +0000

    Added some functionality to updatePassword

--- client/src/Pages/ChangePassword.js
+++ client/src/Pages/ChangePassword.js
@@ -1,22 +1,55 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 
 function ChangePassword() {
-  //send email verification 
-  const sendEmailVerification = () => {
-    Axios.post("http://localhots:3001/2FAEmailVerificationSend", {
+  const [userVerification, setUserVerification] = useState("");
+  const [oldPassword, setOldPassword] = useState("");
+  const [passwordStatus, setPasswordStatus] = useState(false);
+  const [emailStatus, setEmailStatus] = useState(false);
 
-    })
-  }
+  //send email verification
+  const sendEmailVerification = () => {
+    Axios.post("http://localhots:3001/2FAEmailVerificationSend", {});
+  };
 
   //check email verification
+  const emailVerification = () => {
+    Axios.post("http://localhost:3001/VerifyEmail2FA", {
+      passcode: userVerification,
+    }).then((response) => {
+      if (!response.data.auth) {
+        setEmailStatus(false);
+      } else {
+        setEmailStatus(true);
+        
+      }
+    });
+  };
+   //check old password is match for old password
+  const checkOldPass = () => {
+    Axios.post("/checkChangePass", {
+      oldPassword: oldPassword
+    }).then((response) => {
+      if (!response.data.auth) {     
+        setPasswordStatus(false);
+      } else {
+        setPasswordStatus(true);
+      }
+    });
 
-  //check old password is match for old password
+  };
 
-  //if both above are true then update user password 
+ 
+
+  //if both above are true then update user password
+
+  //TODO- pass user input for email verifasction to setUserVerification
+  //    = pass old password to setOldPassword
+
   return (
     <div>
       <h3>Please enter email verification</h3>
+
       <input type="" id="emailVerification" name="emailVerification"></input>
       <div>
         <h3>Please enter old password</h3>
