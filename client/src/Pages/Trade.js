ff296ba85e73d1b397121325dca2f1c41d8a581e
mekeirc <mekeirc@gmail.com>
2022-02-28 17:49:16 +0000

    Step trade page

--- client/src/Pages/Trade.js
+++ client/src/Pages/Trade.js
@@ -8,6 +8,7 @@ import GradientButton from "../Components/GradientButton";
 import PrimaryButton, { SecondaryButton } from "../Components/Buttons";
 import { FormInput, StyledLabel, FormCheckbox } from "../Components/FormInputs";
 import { useNavigate, useLocation } from "react-router-dom";
+import Card from '../Components/Card';
 
 const SwitchButton = styled.button(({ theme }) => css`
 	width: 50px;
@@ -76,12 +77,14 @@ const HighlightedText = styled.span(({ theme }) => css`
 
 const solQuantity = 2;
 
-const Offer = () => {
+const Trade = () => {
 	const [solGbp, setSolGbp] = useState("");
 
 	const [registeredDate, setRegisteredDate] = useState("");
 	const [feedbackScore, setFeedbackScore] = useState("");
 	const [escrowReleaseTime, setEscrowReleaseTime] = useState("");
+
+	const [pageMode, setPageMode] = useState("buy");
 
 	const { state } = useLocation();
 	const { val } = state;
@@ -107,71 +110,138 @@ const Offer = () => {
 
   	return (
 		<PageBody>
-			<div className="container">
-				<div className="row pt-5">
-					<div className="col-12 mb-5 pb-5">
-						<Heading size="26px" className="mb-4">Offers &gt; Buy SOL from {val.userName} with {val.paymentMethod1}.</Heading>
-					</div>
-					<div className="col-12 col-md-6 row">
-						<div className="col-10">
-							Message Area
+			{pageMode === 'buy' && (
+				<div className="container">
+					<div className="row pt-5">
+						<div className="col-12 mb-5 pb-5">
+							<Heading size="26px" className="mb-4">Offers &gt; Buy SOL from {val.userName} with {val.paymentMethod1}.</Heading>
 						</div>
-					</div>
-					<div className="col-1 d-flex justify-content-center">
-						<VerticalDivider />
-					</div>
-					<div className="col-12 col-md-5 row mt-4">
-						<div className="col-12 text-center">
-							<div className="d-flex">
-								<Heading className="me-2">Buying</Heading>
-								<Heading bold>{solQuantity} SOL</Heading>
-								<Heading className="mx-2">for</Heading>
-								<Heading bold>{state.solGbp * solQuantity}</Heading>
+						<div className="col-12 col-md-6 row">
+							<div className="col-10">
+								Message Area
 							</div>
-							<Paragraph size="18px" className="pb-3">1 SOL = {convertCurrencyToSymbol(state.currency)}{state.solGbp}</Paragraph>
-							<HorizontalDivider />
-							<div className="d-flex justify-content-center flex-column">
-								<Paragraph bold size="24px" className="me-2">Waiting for payment from the buyer</Paragraph>
-								<Paragraph size="18px" className="me-2 py-3">
-									Your SOL is now secured in escrow!
-									Please ensure <HighlightedText className="me-2">you have received the payment</HighlightedText>
-									before continuing.
-								</Paragraph>
-								<div className="d-flex text-start">
-									<FormCheckbox type="checkbox" id="checkedPayment" name="checkedPayment" className="me-4" />
-									<StyledLabel className="p-0" htmlFor="checkedPayment">
-									<HighlightedText className="me-1">YES!</HighlightedText> I have confirmed that payment from
-									the buyer is received and checked.
-									</StyledLabel>
+						</div>
+						<div className="col-1 d-flex justify-content-center">
+							<VerticalDivider />
+						</div>
+						<div className="col-12 col-md-5 row mt-4">
+							<div className="col-12 text-center">
+								<div className="d-flex">
+									<Heading className="me-2">Buying</Heading>
+									<Heading bold>{solQuantity} SOL</Heading>
+									<Heading className="mx-2">for</Heading>
+									<Heading bold>{state.solGbp * solQuantity}</Heading>
 								</div>
-								<div className="row mt-5">
-									<div className="col-6">
-										<SecondaryButton
-											text="Cancel"
-											className="m-auto mt-3"
-											onClick={null}
-											type="check"
-											value="check"
-										/>
+								<Paragraph size="18px" className="pb-3">1 SOL = {convertCurrencyToSymbol(state.currency)}{state.solGbp}</Paragraph>
+								<HorizontalDivider />
+								<div className="d-flex justify-content-center flex-column">
+									<Paragraph bold size="24px" className="me-2">Please pay {convertCurrencyToSymbol(state.currency)}{' '}{state.solGbp * solQuantity}</Paragraph>
+									<Paragraph size="18px" className="me-2">
+										into
+									</Paragraph>
+									<Card className="p-3 mb-4">
+										Bank Details Here
+									</Card>
+									<div className="d-flex text-start">
+										<FormCheckbox type="checkbox" id="checkedPayment" name="checkedPayment" className="me-4" />
+										<StyledLabel className="p-0" htmlFor="checkedPayment">
+										<HighlightedText className="me-1">YES!</HighlightedText> I have sent the payment to the seller.
+										</StyledLabel>
 									</div>
-									<div className="col-6">
-										<PrimaryButton
-											text="Continue"
-											className="m-auto mt-3"
-											onClick={null}
-											type="check"
-											value="check"
-											hasIcon
-										/>
+									<div className="row mt-5">
+										<div className="col-6">
+											<SecondaryButton
+												text="Cancel"
+												className="m-auto mt-3"
+												onClick={null}
+												type="check"
+												value="check"
+											/>
+										</div>
+										<div className="col-6">
+											<PrimaryButton
+												text="Continue"
+												className="m-auto mt-3"
+												onClick={null}
+												type="check"
+												value="check"
+												hasIcon
+											/>
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			)}
+			{pageMode === 'sell' && (
+				<div className="container">
+					<div className="row pt-5">
+						<div className="col-12 mb-5 pb-5">
+							<Heading size="26px" className="mb-4">Offers &gt; Sell SOL to {val.userName} with {val.paymentMethod1}.</Heading>
+						</div>
+						<div className="col-12 col-md-6 row">
+							<div className="col-10">
+								Message Area
+							</div>
+						</div>
+						<div className="col-1 d-flex justify-content-center">
+							<VerticalDivider />
+						</div>
+						<div className="col-12 col-md-5 row mt-4">
+							<div className="col-12 text-center">
+								<div className="d-flex">
+									<Heading className="me-2">Selling</Heading>
+									<Heading bold>{solQuantity} SOL</Heading>
+									<Heading className="mx-2">for</Heading>
+									<Heading bold>{state.solGbp * solQuantity}</Heading>
+								</div>
+								<Paragraph size="18px" className="pb-3">1 SOL = {convertCurrencyToSymbol(state.currency)}{state.solGbp}</Paragraph>
+								<HorizontalDivider />
+								<div className="d-flex justify-content-center flex-column">
+									<Paragraph bold size="24px" className="me-2">Waiting for payment from the buyer</Paragraph>
+									<Paragraph size="18px" className="me-2 py-3">
+										Your SOL is now secured in escrow!
+										Please ensure <HighlightedText className="me-2">you have received the payment</HighlightedText>
+										before continuing.
+									</Paragraph>
+									<div className="d-flex text-start">
+										<FormCheckbox type="checkbox" id="checkedPayment" name="checkedPayment" className="me-4" />
+										<StyledLabel className="p-0" htmlFor="checkedPayment">
+										<HighlightedText className="me-1">YES!</HighlightedText> I have confirmed that payment from
+										the buyer is received and checked.
+										</StyledLabel>
+									</div>
+									<div className="row mt-5">
+										<div className="col-6">
+											<SecondaryButton
+												text="Cancel"
+												className="m-auto mt-3"
+												onClick={null}
+												type="check"
+												value="check"
+											/>
+										</div>
+										<div className="col-6">
+											<PrimaryButton
+												text="Continue"
+												className="m-auto mt-3"
+												onClick={null}
+												type="check"
+												value="check"
+												hasIcon
+											/>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
 		</PageBody>
   	);
 }
 
-export default Offer;
+export default Trade;
