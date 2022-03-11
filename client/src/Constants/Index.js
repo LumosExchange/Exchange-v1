56910fb3f25dbc42d6a320fe6ba9952a553582d7
mekeirc <mekeirc@gmail.com>
2022-03-11 16:50:08 +0000

    Update account yupgrade, add completed step upgradeGold doesn't work?

--- client/src/Constants/Index.js
+++ client/src/Constants/Index.js
@@ -1,9 +1,11 @@
 export const birthDayOptions = [
+	"---",
 	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
 	23, 24, 25, 26, 27, 28, 29, 30, 31
 ];
   
 export const birthMonthOptions = [
+	"---",
 	"January",
 	"February",
 	"March",
@@ -23,6 +25,7 @@ export const birthYearOptions = (start, stop, step) =>
 Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
 
 export const countryOptions = [
+	"Please Select",
 	"Afghanistan",
 	"Albania",
 	"Algeria",
