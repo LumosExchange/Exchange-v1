7cf560133526970633275a9006da630f39a43d69
Harry Greenway <harrygreenway96@gmail.com>
2022-04-07 14:00:14 +0100

    added stuff for web3

--- client/src/Pages/WalletTesting.js
+++ client/src/Pages/WalletTesting.js
@@ -1,99 +1,93 @@
-import { useEffect } from 'react';
-import {
-  Program,
-  Provider,
-  BN,
-  web3,
-} from '@project-serum/anchor'
-import {
-  PublicKey
-} from '@solana/web3.js'
+import * as web3 from '@solana/web3.js';
 
 
-import idl from '../../package.json'
-
-const {Keypair, Transaction, SystemProgram, LAMPORTS_PER_SOL} = require("@solana/web3.js");
-const {sendAndConfirmTransaction, clusterApiUrl, Connection} = require("@solana/web3.js");
-
-const opts = {
-  preflightCommitment: "recent",
-};
 
 
-const programID = new PublicKey("GAECQos3deHaqzB1EDvPJcqaGVvG9xqDuFYU239KAsXV")
+
 
 function WalletTesting() {
-  useEffect(() => {
-    window.solana && window.solana.on("connect", () => {
-      console.log('updated...')
-      console.log('public key: ', window.solana.publicKey.toString());
-    })
-    return () => {
-      window.solana.disconnect();
+  const getProvider = async () => {
+    if ("solana" in window) {
+
+      // opens wallet to connect to
+      await window.solana.connect(); 
+
+      const provider = window.solana;
+      if (provider.isPhantom) {
+        console.log("Is Phantom installed?  ", provider.isPhantom);
+        return provider;
+      }
+    } else {
+      window.open("https://www.phantom.app/", "_blank");
     }
-  }, [])
+  };
 
-  useEffect(() => {}, []);
+  async function transferSOL() {
+    // Detecing and storing the phantom wallet of the user (creator in this case)
+    var provider = await getProvider();
+    console.log("Public key of the emitter: ",provider.publicKey.toString());
 
-  async function sendTransaction() {    
-    const wallet = window.solana
-    const network = clusterApiUrl("devnet")
-    let keypair = Keypair.generate();
-const transaction = new Transaction();
+    // Establishing connection 
 
- transaction.add(
-    SystemProgram.transfer({
-      fromPubkey: window.solana.publicKey,
-      toPubkey: keypair.publicKey,
-      lamports: LAMPORTS_PER_SOL
-    })
+      var connection = new web3.Connection(
+        web3.clusterApiUrl('devnet'),
+      );
+
+   
+  
+    // I have hardcoded my secondary wallet address here. You can take this address either from user input or your DB or wherever
+    var recieverWallet = new web3.PublicKey("6ftMNVRAFAc1Wb372xg3Kq6zZ8rhr6b7DGyuhgSELwZ2");
+
+    // Airdrop some SOL to the sender's wallet, so that it can handle the txn fee
+    var airdropSignature = await connection.requestAirdrop(
+      provider.publicKey,
+      web3.LAMPORTS_PER_SOL,
+    );
+
+    // Confirming that the airdrop went through
+    await connection.confirmTransaction(airdropSignature);
+    console.log("Airdropped");
+try {
+  var transaction = new web3.Transaction().add(
+    web3.SystemProgram.transfer({
+      fromPubkey: provider.publicKey,
+      toPubkey: recieverWallet,
+      lamports: web3.LAMPORTS_PER_SOL //Investing 1 SOL. Remember 1 Lamport = 10^-9 SOL.
+    }),
   );
 
-
-  let anotherKeypair = Keypair.generate();
-let connection = new Connection(clusterApiUrl('devnet'));
-  sendAndConfirmTransaction(
-    connection,
-    transaction,
-    [keypair, anotherKeypair]
-  );
-  }
+} finally {
+  
+}
 
 
+    // Setting the variables for the transaction
+    transaction.feePayer = await provider.publicKey;
+    let blockhashObj = await connection.getRecentBlockhash();
+    transaction.recentBlockhash = await blockhashObj.blockhash;
 
-
-
-  async function read() {
-    try {
-      const wallet = window.solana;
-      const network = clusterApiUrl("devnet")
-      const connection = new Connection(network);
-
-      const provider = new Provider(
-        connection, wallet, { commitment: "processed" },
-
-      )
-      console.log('provider: ', provider);
-      
-    } catch (err) {
-      console.log('error: ', err)
+    // Transaction constructor initialized successfully
+    if(transaction) {
+      console.log("Txn created successfully");
     }
+    
+    // Request creator to sign the transaction (allow the transaction)
+    let signed = await provider.signTransaction(transaction);
+    // The signature is generated
+    let signature = await connection.sendRawTransaction(signed.serialize());
+    // Confirm whether the transaction went through or not
+    await connection.confirmTransaction(signature);
+
+    //Signature
+    console.log("Signature: ", signature);
   }
-  async function getWallet() {
-    try {
-      const wallet = typeof window !== 'undefined' && window.solana;
-      await wallet.connect()
-    } catch (err) {
-      console.log('err: ', err)
-    }
-  }
+ 
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={getWallet}>getWallet</button>
-        <button onClick={sendTransaction}>sendTransaction</button>
-        <button onClick={read}>read</button>
+        <button onClick={getProvider()}>getWallet</button>
+        <button onClick={transferSOL()}>sendTransaction</button>
       </header>
     </div>
   );
