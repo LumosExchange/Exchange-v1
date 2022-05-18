548aa80512cb744179e17b4ada3cdc562e006630
Harry Greenway <harrygreenway96@gmail.com>
2022-05-18 14:55:48 +0100

    Started building the new mywallet page

--- client/src/Pages/MyWallet.js
+++ client/src/Pages/MyWallet.js
@@ -4,190 +4,260 @@ import { PageBody, FormInput, StyledDropdown } from "../Components/FormInputs";
 import Heading from "../Components/Heading";
 import Paragraph from "../Components/Paragraph";
 import PrimaryButton, { InvisibleButton } from "../Components/Buttons";
-import Card from "../Components/Card";
+import PhantomIcon from "../Images/phantom-icon-purple.svg";
+import SolflareIcon from "../Images/solflare-icon.svg";
+import Link from "../Components/Link";
 import GradientButton from "../Components/GradientButton";
-import GradientCard from "../Components/GradientCard";
-import { Collapse } from "@material-ui/core";
-import SolanaIcon from '../Images/icon-solana.svg';
-import DropdownIcon from '../Images/icon-dropdown.svg';
-import StyledTable from "../Components/Tables";
-import { convertAssetToIcon } from '../Components/AirDrops';
+import * as web3 from '@solana/web3.js';
+import SlopeIcon from '../Images/slope-finance-icon.png';
 
+const ToggleIconBase = styled.svg(
+  ({ toggled, theme }) => css`
+    transform: ${toggled && "rotate(180deg)"};
 
-const ToggleIconBase = styled.svg(({ toggled, theme }) => css`
-	transform: ${toggled && 'rotate(180deg)'};
+    &.small {
+      width: 40px;
+      min-height: 40px;
+      min-width: 40px;
+    }
 
-	&.small {
-		width: 40px;
-		min-height: 40px;
-		min-width: 40px;
-	}
-
-	circle, path {
-		stroke: ${theme.colors.text_primary};
-	}
-`);
-
-export const ToggleIcon = ({ className, toggled }) => (
-	<ToggleIconBase
-		width="71"
-		height="71"
-		viewBox="0 0 71 71" fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-		className={className}
-		toggled={toggled}
-	>
-		<circle cx="35.5" cy="35.5" r="34" strokeWidth="3"/>
-		<path d="M25.5 33L36.5 44L47.5 33" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
-	</ToggleIconBase>
+    circle,
+    path {
+      stroke: ${theme.colors.text_primary};
+    }
+  `
 );
 
-const Divider = styled.hr(({ theme }) => css`
+const WalletCard = styled.span(
+  ({ theme }) => css`
+    background: ${theme.colors.card_bg};
+    border-radius: 10px;
+    cursor: pointer;
+  `
+);
+
+const StyledRadio = styled.input(
+  ({ theme }) => css`
+    display: none;
+
+    &:checked + div {
+      background: -webkit-linear-gradient(
+        300deg,
+        #fce608,
+        #ff7586,
+        #b372ce,
+        #6f86ff
+      );
+      border-radius: 10px;
+      padding: 2px 4px 4px 2px;
+    }
+  `
+);
+
+const ConnectToggle = ({ id, image, wallet, className, onClick }) => (
+  <div className={className} onClick={onClick}>
+    <label htmlFor={id} className="w-100 text-center">
+      <StyledRadio id={id} type="radio" name="walletSelection" />
+      <div>
+        <WalletCard className="d-flex p-4 flex-column">
+          <img
+            src={image}
+            alt={wallet}
+            className="pb-3 m-auto"
+            style={{ width: "70px" }}
+          />
+          <Heading size="24px">{wallet}</Heading>
+        </WalletCard>
+      </div>
+    </label>
+  </div>
+);
+
+export const ToggleIcon = ({ className, toggled }) => (
+  <ToggleIconBase
+    width="71"
+    height="71"
+    viewBox="0 0 71 71"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    toggled={toggled}
+  >
+    <circle cx="35.5" cy="35.5" r="34" strokeWidth="3" />
+    <path
+      d="M25.5 33L36.5 44L47.5 33"
+      strokeWidth="3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </ToggleIconBase>
+);
+
+const Divider = styled.hr(
+  ({ theme }) => css`
 	background ${theme.colors.secondary_link};
 	width: 100%;
 	opacity: 1;
-`);
+`
+);
 
-const RewardPools = [
-	{
-		'currency': 'SOL',
-		'amount': '5',
-	},
-	{
-		'currency': 'LRA',
-		'amount': '100',
-	},
-	{
-		'currency': 'COPE',
-		'amount': '12',
-	},
-];
+const IconContainer = styled.div(
+  ({ theme }) => css`
+    img,
+    svg {
+      width: 100%;
+      height: 117px;
+    }
+  `
+);
 
-const FakeTableData = [
-	{
-		'provider': 'shinji0314',
-		'region': 'United Kingdom',
-		'date': '16-07-2021 - 11:00:00',
-		'type': 'Airdrop',
-		'amount': 0.5,
-		'currency': 'SOL',
-	},
-	{
-		'provider': 'asuka',
-		'region': 'Germany',
-		'date': '15-07-2021 - 09:30:00',
-		'type': 'Airdrop',
-		'amount': 1,
-		'currency': 'SOL',
-	},
-	{
-		'provider': 'johnsmith05',
-		'region': 'United Kingdom',
-		'date': '15-07-2021 - 09:30:00',
-		'type': 'Airdrop',
-		'amount': 1,
-		'currency': 'COPE',
-	},
-	{
-		'provider': 'user4578',
-		'region': 'Spain',
-		'date': '15-07-2021 - 09:30:00',
-		'type': 'Airdrop',
-		'amount': 1,
-		'currency': 'LRA',
-	},
-];
+const pubKey = "GAECQos3deHaqzB1EDvPJcqaGVvG9xqDuFYU239KAsXV";
 
-const IconContainer = styled.div(({ theme }) => css`
-	img, svg {
-		width: 100%;
-		height: 117px;
-	}
-`);
-
-const pubKey="GAECQos3deHaqzB1EDvPJcqaGVvG9xqDuFYU239KAsXV";
+//TODO:
+// - On page load connect to user web3 wallet
+// - Then get pub key of wallet
+// - then get solana balnace / other token balance and map to table
+// - then get all NFT's in the users wallet and display below
 
 const MyWallet = () => {
-  const [selectedCrypto, setSelectedCrypto] = useState("SOL");
+  const [selectedWallet, selectWallet] = useState("");
+  const [currentStep, setCurrentStep] = useState("connectWallet");
+
+  const pubKey = "";
+
+
+  //Get provider 
+  const getProvider = async () => {
+    if ("solana" in window) {
+
+      // opens wallet to connect to
+      await window.solana.connect(); 
+
+      const provider = window.solana;
+	  if (provider.isPhantom) {
+		console.log("Is Phantom installed? ", provider.isPhantom);
+		setCurrentStep("walletOverview");
+		return provider;
+	} if (provider.isSolflare){
+		console.log("Is Solflare installed? ", provider.isSolflare);
+		setCurrentStep("walletOverview");
+		return provider;
+	} if (provider.isSlope) {
+		console.log("Is Slope installed? ", provider.isSlope);
+		setCurrentStep("walletOverview");
+		return provider;
+	}  
+    } else {
+      window.open("https://www.phantom.app/", "_blank");
+    }
+  };
 
   return (
-		<PageBody className="d-flex align-items-start pt-5">
-			<div className="container d-flex align-items-center justify-content-center py-5 flex-column">
-				<div className="row w-100">
-					<div className="col-12">
-						<div className="flex-column">
-							<Heading className="pb-3">Your Wallet</Heading>
-							<Paragraph size="18px" bold>
-								Manage your credits, and grab a chance to earn with our reward pool.
-							</Paragraph>
-						</div>
-					</div>
-					<div className="col-12 col-lg-6">
-						<Card className="p-4 flex-column">
-							<Heading size="24px" color="text_primary" bold>Select Crypto</Heading>
-							<div className="d-flex justify-content-between align-items-center">
-								<StyledDropdown
-									className="w-100"
-									placeholder="Please Select"
-									name="preferredPayment"
-									value={selectedCrypto}
-									id="preferredPayment"
-									onChange={(e) => {
-										setSelectedCrypto(e.target.value);
-									}}
-								>
-									<option value="SOL">Solana (SOL)</option>
-									<option value="LRA">Lumos Rewards (LRA)</option>
-									<option value="COPE">COPE (COPE)</option>
-								</StyledDropdown>
-							</div>
-						</Card>
-					</div>
-					<div className="col-12 col-lg-6">
-						<Card className="p-4 flex-column">
-							<Heading size="24px" color="text_primary" bold>Reward Pool</Heading>
-							<div className="d-flex justify-content-between align-items-center">
-								<Heading size="28px" className="mb-0">{RewardPools.filter(rp => rp.currency === selectedCrypto).map(rp => rp.amount)} {selectedCrypto}</Heading>
-								<GradientButton text="Claim" fontSize="20px" />
-							</div>
-						</Card>
-					</div>
-				</div>
-				<div className="row w-100 mt-4">
-					<div className="col-12 d-flex flex-column">
-						<Divider />
-						<Heading size="24px" className="mb-0 pt-4">Reward History</Heading>
-					</div>
-					<StyledTable className="w-100 mt-4">
-						<thead>
-							<tr>
-								<th>Provider</th>
-								<th>Region</th>
-								<th>Date</th>
-								<th>Type</th>
-								<th>Amount</th>
-							</tr>
-						</thead>
-						<tbody>
-							{selectedCrypto && FakeTableData.filter(fd => fd.currency === selectedCrypto).map((data, d) => (
-								<tr key={d}>
-									<td>
-										<span>
-											{data.provider}
-										</span>
-									</td>
-									<td>{data.region}</td>
-									<td>{data.date}</td>
-									<td>{data.type}</td>
-									<td>{data.amount} {data.currency}</td>
-								</tr>
-							))}
-						</tbody>
-					</StyledTable>
-				</div>
-			</div>
-    	</PageBody>
+    <PageBody className="d-flex align-items-center">
+      <div className="container d-flex align-items-center justify-content-center py-5 flex-column">
+        <Heading className="pb-4 text-center">
+          Connect Using a web3 Wallet
+        </Heading>
+        <Heading size="24px" className="text-center">
+          Please select your wallet
+        </Heading>
+        <div className="row p-5 d-flex justify-content-center w-100">
+          <div className="col-12 col-sm-5 col-lg-4 col-xl-4 col-xxl-2">
+            <ConnectToggle
+              wallet="Phantom"
+              image={PhantomIcon}
+              onClick={() => selectWallet("phantom")}
+            />
+          </div>
+          <div className="col-12 col-sm-5 col-lg-4 col-xl-4 col-xxl-2 my-3 my-sm-0">
+            <ConnectToggle
+              wallet="Solflare"
+              image={SolflareIcon}
+              onClick={() => selectWallet("solflare")}
+            />
+          </div>
+          <div className="col-12 col-sm-5 col-lg-4 col-xl-4 col-xxl-2 mt-sm-3 mt-lg-0">
+            <ConnectToggle
+              wallet="Slope"
+              image={SlopeIcon}
+              onClick={() => selectWallet("Slope")}
+            />
+          </div>
+          <div className="col-12 col-lg-6 col-xl-8 d-flex mt-5">
+            {selectedWallet === "phantom" && (
+              <Paragraph size="18px">
+                <Link href="https://phantom.app" alt="Phantom" target="_blank">
+                  Phantom
+                </Link>{" "}
+                is a friendly non-custodial, browser extension, Solana wallet
+                that makes it safe & easy for you to store, send, receive,
+                collect, and swap tokens.
+                <PrimaryButton
+                  text="Connect"
+                  className="m-auto mt-3"
+                  onClick={() => {getProvider();}}
+                  type="check"
+                  value="check"
+                  hasIcon
+                />
+              </Paragraph>
+            )}
+            {selectedWallet === "solflare" && (
+              <Paragraph size="18px">
+                <Link
+                  href="https://solflare.com"
+                  alt="Solflare"
+                  target="_blank"
+                >
+                  Solflare
+                </Link>{" "}
+                is a friendly non-custodial, browser extension, Solana wallet
+                that makes it safe & easy for you to store, send, receive,
+                collect, and swap tokens.
+                <PrimaryButton
+                  text="Connect"
+                  className="m-auto mt-3"
+                  onClick={() => {getProvider();}}
+                  type="check"
+                  value="check"
+                  hasIcon
+                />
+              </Paragraph>
+            )}
+            {selectedWallet === "Slope" && (
+              <Paragraph size="18px">
+                <Link
+                  href="https://www.slope.com/"
+                  alt="Slope"
+                  target="_blank"
+                >
+                  Slope
+                </Link>{" "}
+                is a friendly non-custodial, browser extension, Solana wallet
+                that makes it safe & easy for you to store, send, receive,
+                collect, and swap tokens.
+                <PrimaryButton
+                  text="Connect"
+                  className="m-auto mt-3"
+                  onClick={() => {getProvider();}}
+                  type="check"
+                  value="check"
+                  hasIcon
+                />
+              </Paragraph>
+            )}
+			{currentStep === "walletOverview" && (
+				<Paragraph>
+					Hello
+				</Paragraph>
+
+			)}
+
+          </div>
+        </div>
+      </div>
+    </PageBody>
   );
-}
+};
 
 export default MyWallet;
