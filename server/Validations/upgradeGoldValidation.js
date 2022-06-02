325c4e212e162163dc075afe669758f5ef76be2c
Harry Greenway <harrygreenway96@gmail.com>
2022-06-02 13:24:26 +0100

    building more API validation

--- server/Validations/upgradeGoldValidation.js
+++ server/Validations/upgradeGoldValidation.js
@@ -1,7 +1,20 @@
 const yup = require('yup');
 
 const goldUpgrade = yup.object({
-
+    file: yup
+    .require(),
+    EmployerName: yup
+    .string()
+    .require(),
+    EmployerAddress: yup
+    .require()
+    .string(),
+    Occupation: yup
+    .require()
+    .string(),
+    Income: yup
+    .require()
+    .string(),
 });
 
 goldUpgrade.validate();
--- server/Validations/upgradeSilverValidation.js
+++ server/Validations/upgradeSilverValidation.js
@@ -1,7 +1,17 @@
 const yup = require('yup');
 
 const silverUpgrade = yup.object({
-
+    file: yup
+    .require(),
+    birthDay: yup
+    .number()
+    .require(),
+    birthMonth: yup
+    .number()
+    .require(),
+    birthYear: yup
+    .number()
+    .require(),
 });
 
 silverUpgrade.validate();
